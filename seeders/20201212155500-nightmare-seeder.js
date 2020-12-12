c4c6e71ec87685c33d5244b8837896cf9e299ec4
Melanie Stuart <mmooradian90@yahoo.com>
2020-12-12 11:40:31 -0500

    pushing seed files.

---
+++ seeders/20201212155500-nightmare-seeder.js
@@ -0,0 +1,73 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+ 
+      await queryInterface.bulkInsert('nightmare', [{
+        quote: 'The more you sweat in training, the less you bleed in combat. --Navy SEAL’s',
+        createdAt: new date(),
+        updatedAt: new Date()
+      
+      },{
+        quote: 'The pain of discipline is nothing like the pain of disappointment --Justin Langer',
+        createdAt: new date(),
+        updatedAt: new Date()
+      
+      },{
+        quote: 'Shut up and train',
+        createdAt: new date(),
+        updatedAt: new Date()
+      
+      },{
+        quote: 'I don’t care what you USED to bench. --Serge Ceralde',
+        createdAt: new date(),
+        updatedAt: new Date()
+      
+      },{
+        quote: 'The fight is won or lost far away from witnesses, behind the lines, in the gym, and out there on the road, long before I dance under those lights. --Muhammad Ali',
+        createdAt: new date(),
+        updatedAt: new Date()
+      
+      },{
+        quote: 'When you come to the end of your rope, tie a knot and hang on. -- Franklin D. Roosevelt',
+        createdAt: new date(),
+        updatedAt: new Date()
+      
+      },{
+        quote: 'Not everyone is capable of madness; and of those lucky enough to be capable, not many have the courage for it.',
+        createdAt: new date(),
+        updatedAt: new Date()
+      
+      },{
+        quote: 'Pain is weakness leaving the body',
+        createdAt: new date(),
+        updatedAt: new Date()
+      
+      },{
+        quote: 'Go Hard, or Go Home',
+        createdAt: new date(),
+        updatedAt: new Date()
+      
+      },{
+        quote: 'Take care of your body. It’s the only place you have to live. --Jim Rohn',
+        createdAt: new date(),
+        updatedAt: new Date()
+      
+      },
+
+      
+    ], 
+      
+      {});
+    
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
