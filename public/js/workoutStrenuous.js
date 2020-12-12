d387a4756a61ee81c66687091c9565650213ebd9
Maria Byler <mariabyler98@gmail.com>
2020-12-12 14:12:19 -0500

    added individual html and js files for all the workout options

---
+++ public/js/workoutStrenuous.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+    const youtubeDiv = $('#youtubeDiv');
+    const completeBtn = $('#completeBtn'); 
+    const daysCompleteDiv = $('#daysCompleteDiv'); 
+    const quoteDiv = $('#quoteDiv');  
+});
