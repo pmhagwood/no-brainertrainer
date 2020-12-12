bf5e42dabbfba714389814330a7e2c4bfe4c2b73
Melanie Stuart <mmooradian90@yahoo.com>
2020-12-12 12:50:45 -0500

    fixing user seeder.

--- seeders/20201212155527-user-seeder.js
+++ seeders/20201212155527-user-seeder.js
@@ -1,28 +1,3 @@
-// 'use strict';
-
-// module.exports = {
-//   up: async (queryInterface, Sequelize) => {
-//     /**
-//      * Add seed commands here.
-//      *
-//      * Example:
-//      * await queryInterface.bulkInsert('People', [{
-//      *   name: 'John Doe',
-//      *   isBetaMember: false
-//      * }], {});
-//     */
-//   },
-
-//   down: async (queryInterface, Sequelize) => {
-//     /**
-//      * Add commands to revert seed here.
-//      *
-//      * Example:
-//      * await queryInterface.bulkDelete('People', null, {});
-//      */
-//   }
-// };
-
 
 'use strict';
 
@@ -40,25 +15,25 @@ module.exports = {
       email: 'mariabyler98@gmail.com',
       createdAt: new Date(),
       updatedAt: new Date()
-    }{
+    },{
       userName: 'pmhagwood',
       password: 'password',
       email: '',
       createdAt: new Date(),
       updatedAt: new Date()
-    }{
+    },{
       userName: 'Rachelmoore2020',
       password: 'password',
       email: 'rachelmoore6822@gmail.com',
       createdAt: new Date(),
       updatedAt: new Date()
-    }{
+    },{
       userName: 'duncangw10',
       password: 'password',
       email: 'duncangw1@gmail.com',
       createdAt: new Date(),
       updatedAt: new Date()
-    }{
+    },{
       userName: 'DigitalAmnesia',
       password: 'password',
       email: 'mike@digitalamnesia.io',
