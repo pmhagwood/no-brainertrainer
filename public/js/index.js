f7a773c4d6ccc82343d102bf904473c44d909fea
Paul Hagwood <SubtleKnife@Pauls-MacBook-Pro.local>
2020-12-12 12:39:25 -0500

    updated html index

---
+++ public/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+    $(".btn-accout").click(function (event) {
+        event.preventDefault();
+        console.log("clicked");
+        // const go_to_url = $("/signup").find(":selected").val();
+
+        //this will redirect us in same window
+        document.location.href = "/signup";
+    });
+});
