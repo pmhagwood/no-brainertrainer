f666f537f1d618225b1e1b784bc9cdeeb48265b8
Melanie Stuart <mmooradian90@yahoo.com>
2020-12-12 11:49:30 -0500

    seeding the user table.

--- seeders/20201212155527-user-seeder.js
+++ seeders/20201212155527-user-seeder.js
@@ -1,24 +1,74 @@
+// 'use strict';
+
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+//     /**
+//      * Add seed commands here.
+//      *
+//      * Example:
+//      * await queryInterface.bulkInsert('People', [{
+//      *   name: 'John Doe',
+//      *   isBetaMember: false
+//      * }], {});
+//     */
+//   },
+
+//   down: async (queryInterface, Sequelize) => {
+//     /**
+//      * Add commands to revert seed here.
+//      *
+//      * Example:
+//      * await queryInterface.bulkDelete('People', null, {});
+//      */
+//   }
+// };
+
+
 'use strict';
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert('Users', [{
+      userName: 'MStuart90',
+      password: 'password',
+      email: 'mmooradian90@yahoo.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      userName: 'mariabyler',
+      password: 'password',
+      email: 'mariabyler98@gmail.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }{
+      userName: 'pmhagwood',
+      password: 'password',
+      email: '',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }{
+      userName: 'Rachelmoore2020',
+      password: 'password',
+      email: 'rachelmoore6822@gmail.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }{
+      userName: 'duncangw10',
+      password: 'password',
+      email: 'duncangw1@gmail.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }{
+      userName: 'DigitalAmnesia',
+      password: 'password',
+      email: 'mike@digitalamnesia.io',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+    
+  ]);
   },
-
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('Users', null, {});
   }
 };
